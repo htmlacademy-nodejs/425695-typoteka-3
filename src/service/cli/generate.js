1cb93439639bc38caa6a4721e983f8246ae4465c
dimitriskot <mopo_kdi-web@mail.ru>
2021-02-15 19:27:28 +0300

    module2-task2

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -4,46 +4,63 @@ const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 const {
   ANNOUNCE_MAX_COUNT,
-  CATEGORIES,
   CreatedDateMs,
   DEFAULT_COUNT,
   ExitCode,
+  FILE_CATEGORIES_PATH,
   FILE_NAME,
-  FULLTEXT_MAX_COUNT,
-  SENTENCES,
-  TITLES,
+  FILE_SENTENCES_PATH,
+  FILE_TITLES_PATH,
 } = require(`../constants`);
 const {
   getRandomInt,
   shuffle,
 } = require(`../utils`);
 
-const getCategories = (categoriesLength) => {
+const getCategories = (categories) => {
+  const categoriesLength = getRandomInt(1, categories.length - 1);
   return Array.from(new Set(Array(categoriesLength)
   .fill(``)
-  .map(() => CATEGORIES[getRandomInt(0, categoriesLength - 1)])));
+  .map(() => categories[getRandomInt(0, categoriesLength)])));
 };
 
-const generateOffers = (count) => (
+const generateOffers = (count, categories, sentences, titles) => (
   Array(count).fill({}).map(() => ({
-    announce: shuffle(SENTENCES).slice(1, ANNOUNCE_MAX_COUNT).join(` `),
-    сategory: getCategories(getRandomInt(1, CATEGORIES.length)),
+    announce: shuffle(sentences).slice(1, ANNOUNCE_MAX_COUNT).join(` `),
+    сategory: getCategories(categories),
     createdDate: new Date(getRandomInt(CreatedDateMs.MIN, CreatedDateMs.MAX)),
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
-    fullText: shuffle(SENTENCES).slice(1, FULLTEXT_MAX_COUNT).join(` `),
+    title: titles[getRandomInt(0, titles.length - 1)],
+    fullText: shuffle(sentences).slice(1, getRandomInt(0, sentences.length - 1)).join(` `),
   }))
 );
+
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+
+    return content.split(`\n`).filter(Boolean);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
 
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
     if (countOffer > 1000) {
       console.info(chalk.red(`Не больше 1000 объявлений.`));
       process.exit(ExitCode.ERROR);
     }
-    const content = JSON.stringify(generateOffers(countOffer));
+
+    const offers = generateOffers(countOffer, categories, sentences, titles);
+    const content = JSON.stringify(offers);
     try {
       await fs.writeFile(FILE_NAME, content);
       console.info(chalk.green(`Operation success. File created.`));
