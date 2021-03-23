660953f2eee5417b059e6f333504df2c83ce92c4
dimitriskot <mopo_kdi-web@mail.ru>
2021-03-23 15:23:30 +0300

    module3-task2

--- src/express/express.js
+++ src/express/express.js
@@ -1,10 +1,16 @@
 'use strict';
 
+const path = require(`path`);
 const express = require(`express`);
-const {DEFAULT_SERVER_PORT} = require(`./constants`);
+const {DEFAULT_SERVER_PORT, PUBLIC_DIR} = require(`./constants`);
 const {articlesRoutes, categoriesRoutes, mainRoutes, myRoutes} = require(`./routes`);
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.use(`/`, mainRoutes);
 app.use(`/categories`, categoriesRoutes);
